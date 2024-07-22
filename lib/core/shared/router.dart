169e38ea4e32a6f6d556a78e078ce818352b3c0c
Omar khaium Chowhdury <omar.piistech@gmail.com>
2024-07-23 05:34:56 +0600

    refactored

--- lib/core/shared/router.dart
+++ lib/core/shared/router.dart
@@ -1,61 +1,22 @@
-import '../../features/dashboard/dashboard.dart';
+// import '../../features/home/home.dart';
 import '../../features/login/login.dart';
 import '../../features/profile/profile.dart';
 import '../config/config.dart';
 import 'shared.dart';
 
 final router = GoRouter(
-  initialLocation: DashboardPage.path,
+  initialLocation: LoginPage.path,
   routes: [
     GoRoute(
-      name: DashboardPage.name,
-      path: DashboardPage.path,
-      builder: (context, state) {
-        return const DashboardPage();
-      },
-      redirect: (context, state) => context.auth.authenticated ? null : LoginPage.path,
-    ),
-    GoRoute(
-      name: ProfilePage.name,
-      path: ProfilePage.path,
-      builder: (context, state) {
-        return MultiBlocProvider(
-          providers: [
-            BlocProvider(
-              create: (context) => sl<FindProfileBloc>()
-                ..add(
-                  const FindProfile(),
-                ),
-            ),
-          ],
-          child: const ProfilePage(),
-        );
-      },
-      redirect: (context, state) => context.auth.authenticated ? null : LoginPage.path,
-    ),
-    GoRoute(
-      name: LoginPage.name,
       path: LoginPage.path,
-      builder: (context, state) {
-        return MultiBlocProvider(
-          providers: [
-            BlocProvider(create: (context) => sl<LoginBloc>()),
-          ],
-          child: const LoginPage(),
-        );
-      },
-    ),
-    GoRoute(
-      name: ForgotPasswordPage.name,
-      path: ForgotPasswordPage.path,
-      builder: (context, state) {
-        return MultiBlocProvider(
-          providers: [
-            BlocProvider(create: (context) => sl<ForgotPasswordBloc>()),
-          ],
-          child: const ForgotPasswordPage(),
-        );
-      },
+      name: LoginPage.name,
+      builder: (context, state) => MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => sl<CheckProfileBloc>()),
+          BlocProvider(create: (context) => sl<LoginBloc>()),
+        ],
+        child: const LoginPage(),
+      ),
     ),
   ],
 );
