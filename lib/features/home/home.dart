2169ac0242551ea1326ab5beb5a46d451e8f350c
Omar khaium Chowhdury <omar.piistech@gmail.com>
2024-07-23 06:03:13 +0600

    removed unused files

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,17 +1,1 @@
-export 'data/repositories/home_impl.dart';
-export 'data/datasources/remote.dart';
-export 'data/datasources/remote_impl.dart';
-
-export 'data/models/home.dart';
-
-export 'domain/entities/home.dart';
-export 'domain/repositories/home.dart';
-export 'domain/usecases/create.dart';
-export 'domain/usecases/delete.dart';
-export 'domain/usecases/find.dart';
-export 'domain/usecases/read.dart';
-export 'domain/usecases/refresh.dart';
-export 'domain/usecases/search.dart';
-export 'domain/usecases/update.dart';
-
 export 'presentation/pages/home.dart';
