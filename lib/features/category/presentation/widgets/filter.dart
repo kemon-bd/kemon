d8d5cec9803bc6c68f2cee6fccb8f07effbfeff2
omar-khaium <omar.piistech@gmail.com>
2024-11-21 00:28:38 +0600

    logic and ui refactored

--- lib/features/category/presentation/widgets/filter.dart
+++ lib/features/category/presentation/widgets/filter.dart
@@ -13,19 +13,32 @@ class FilterBusinessesByCategoryWidget extends StatefulWidget {
   State<FilterBusinessesByCategoryWidget> createState() => _FilterBusinessesByCategoryWidgetState();
 }
 
+enum RatingRange {
+  all,
+  worst,
+  average,
+  best,
+}
+
 class _FilterBusinessesByCategoryWidgetState extends State<FilterBusinessesByCategoryWidget> {
   LookupEntity? division;
   LookupEntity? district;
   LookupEntity? thana;
   SubCategoryEntity? subCategory;
 
-  final List<int> ratings = [];
+  RatingRange rating = RatingRange.all;
 
   @override
   void initState() {
     super.initState();
     final filter = context.read<FindBusinessesByCategoryBloc>().state;
-    ratings.addAll(filter.ratings);
+    rating = filter.ratings.isEmpty
+        ? RatingRange.all
+        : filter.ratings.contains(1) && filter.ratings.contains(2)
+            ? RatingRange.worst
+            : filter.ratings.contains(3) && filter.ratings.contains(4)
+                ? RatingRange.average
+                : RatingRange.best;
     division = filter.division;
     district = filter.district;
     thana = filter.thana;
@@ -90,9 +103,15 @@ class _FilterBusinessesByCategoryWidgetState extends State<FilterBusinessesByCat
                         child: SubCategoryFilterWidget(subCategory: subCategory, category: state.category.identity.guid),
                       ),
                       onSelect: (selection) {
-                        setState(() {
-                          subCategory = selection;
-                        });
+                        if (selection.urlSlug == subCategory?.urlSlug) {
+                          setState(() {
+                            subCategory = null;
+                          });
+                        } else {
+                          setState(() {
+                            subCategory = selection;
+                          });
+                        }
                       },
                     );
                   } else if (state is FindCategoryLoading) {
@@ -132,120 +151,165 @@ class _FilterBusinessesByCategoryWidgetState extends State<FilterBusinessesByCat
                       child: DivisionFilterWidget(division: division),
                     ),
                     onSelect: (selection) {
-                      setState(() {
-                        division = selection;
-                      });
+                      if (selection.value == division?.value) {
+                        setState(() {
+                          division = null;
+                          district = null;
+                          thana = null;
+                        });
+                      } else {
+                        setState(() {
+                          division = selection;
+                          district = null;
+                          thana = null;
+                        });
+                      }
                     },
                   ),
-                  const Divider(),
-                  DropdownWidget<LookupEntity>(
-                    label: 'District',
-                    labelStyle: TextStyles.subTitle(context: context, color: theme.textSecondary),
-                    text: district?.text ?? 'Select one',
-                    textStyle: TextStyles.title(context: context, color: theme.textPrimary),
-                    popup: BlocProvider(
-                      create: (_) => sl<FindLookupBloc>()
-                        ..add(
-                          FindLookupWithParent(lookup: Lookups.district, parent: division?.value ?? ''),
+                  if (division != null) ...[
+                    const Divider(),
+                    DropdownWidget<LookupEntity>(
+                      label: 'District',
+                      labelStyle: TextStyles.subTitle(context: context, color: theme.textSecondary),
+                      text: district?.text ?? 'Select one',
+                      textStyle: TextStyles.title(context: context, color: theme.textPrimary),
+                      popup: BlocProvider(
+                        create: (_) => sl<FindLookupBloc>()
+                          ..add(
+                            FindLookupWithParent(lookup: Lookups.district, parent: division?.value ?? ''),
+                          ),
+                        child: DistrictFilterWidget(
+                          district: district,
+                          division: division?.value ?? '',
                         ),
-                      child: DistrictFilterWidget(
-                        district: district,
-                        division: division?.value ?? '',
                       ),
+                      onSelect: (selection) {
+                        if (selection.value == district?.value) {
+                          setState(() {
+                            district = null;
+                            thana = null;
+                          });
+                        } else {
+                          setState(() {
+                            district = selection;
+                            thana = null;
+                          });
+                        }
+                      },
                     ),
-                    onSelect: (selection) {
-                      setState(() {
-                        district = selection;
-                      });
-                    },
-                  ),
-                  const Divider(),
-                  DropdownWidget<LookupEntity>(
-                    label: 'Thana',
-                    labelStyle: TextStyles.subTitle(context: context, color: theme.textSecondary),
-                    text: thana?.text ?? 'Select one',
-                    textStyle: TextStyles.title(context: context, color: theme.textPrimary),
-                    popup: BlocProvider(
-                      create: (_) => sl<FindLookupBloc>()
-                        ..add(
-                          FindLookupWithParent(lookup: Lookups.thana, parent: district?.value ?? ''),
+                  ],
+                  if (district != null) ...[
+                    const Divider(),
+                    DropdownWidget<LookupEntity>(
+                      label: 'Thana',
+                      labelStyle: TextStyles.subTitle(context: context, color: theme.textSecondary),
+                      text: thana?.text ?? 'Select one',
+                      textStyle: TextStyles.title(context: context, color: theme.textPrimary),
+                      popup: BlocProvider(
+                        create: (_) => sl<FindLookupBloc>()
+                          ..add(
+                            FindLookupWithParent(lookup: Lookups.thana, parent: district?.value ?? ''),
+                          ),
+                        child: ThanaFilterWidget(
+                          thana: thana,
+                          district: district?.value ?? '',
                         ),
-                      child: ThanaFilterWidget(
-                        thana: thana,
-                        district: district?.value ?? '',
                       ),
+                      onSelect: (selection) {
+                        if (selection.value == thana?.value) {
+                          setState(() {
+                            thana = null;
+                          });
+                        } else {
+                          setState(() {
+                            thana = selection;
+                          });
+                        }
+                      },
                     ),
-                    onSelect: (selection) {
-                      setState(() {
-                        thana = selection;
-                      });
-                    },
-                  ),
+                  ],
                 ],
               ),
             ),
-            const Divider(height: 42),
+            const Divider(height: 24),
             Text(
               'Rating',
               style: TextStyles.subTitle(context: context, color: theme.textPrimary),
             ),
             const SizedBox(height: 8),
-            Container(
-              decoration: BoxDecoration(
-                color: theme.backgroundSecondary,
-                borderRadius: BorderRadius.circular(16),
-                border: Border.all(color: theme.backgroundTertiary, width: .25),
-              ),
-              child: ListView.separated(
-                shrinkWrap: true,
-                padding: const EdgeInsets.symmetric(vertical: 8),
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: 5,
-                separatorBuilder: (context, index) => const Divider(height: 8),
-                itemBuilder: (context, index) {
-                  final rating = 5 - index;
-                  final selected = ratings.contains(rating);
-                  return ListTile(
-                    dense: true,
-                    horizontalTitleGap: 24,
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-                    visualDensity: const VisualDensity(horizontal: -4, vertical: -4),
-                    leading: Container(
-                      width: 24,
-                      height: 24,
-                      decoration: BoxDecoration(
-                        color: selected ? theme.primary : theme.backgroundTertiary,
-                        borderRadius: BorderRadius.circular(8),
-                        border: Border.all(color: theme.textPrimary, width: 1),
+            CupertinoSlidingSegmentedControl<RatingRange>(
+              groupValue: rating,
+              children: {
+                RatingRange.all: Text(
+                  'All',
+                  style: TextStyles.subTitle(
+                    context: context,
+                    color: rating == RatingRange.all ? theme.white : theme.textSecondary,
+                  ),
+                ),
+                RatingRange.worst: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.star_rounded,
+                      color: rating == RatingRange.worst ? theme.white : theme.textSecondary,
+                      size: Dimension.radius.sixteen,
+                    ),
+                    const SizedBox(width: 2),
+                    Text(
+                      '1 ~ 2',
+                      style: TextStyles.subTitle(
+                        context: context,
+                        color: rating == RatingRange.worst ? theme.white : theme.textSecondary,
                       ),
-                      child: selected
-                          ? Icon(
-                              Icons.check_rounded,
-                              size: 16,
-                              color: theme.white,
-                              weight: 700,
-                              grade: 200,
-                            )
-                          : null,
                     ),
-                    title: RatingBarIndicator(
-                      itemBuilder: (_, index) => Icon(Icons.star_rounded, color: theme.primary),
-                      itemSize: 24,
-                      itemCount: rating,
-                      unratedColor: theme.textSecondary,
-                      rating: rating.toDouble(),
+                  ],
+                ),
+                RatingRange.average: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.star_rounded,
+                      color: rating == RatingRange.average ? theme.white : theme.textSecondary,
+                      size: Dimension.radius.sixteen,
                     ),
-                    onTap: () {
-                      if (selected) {
-                        ratings.remove(rating);
-                      } else {
-                        ratings.add(rating);
-                      }
-                      setState(() {});
-                    },
-                  );
-                },
-              ),
+                    const SizedBox(width: 2),
+                    Text(
+                      '3 ~ 4',
+                      style: TextStyles.subTitle(
+                        context: context,
+                        color: rating == RatingRange.average ? theme.white : theme.textSecondary,
+                      ),
+                    ),
+                  ],
+                ),
+                RatingRange.best: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      Icons.star_rounded,
+                      color: rating == RatingRange.best ? theme.white : theme.textSecondary,
+                      size: Dimension.radius.sixteen,
+                    ),
+                    const SizedBox(width: 2),
+                    Text(
+                      '5.0',
+                      style: TextStyles.subTitle(
+                        context: context,
+                        color: rating == RatingRange.best ? theme.white : theme.textSecondary,
+                      ),
+                    ),
+                  ],
+                ),
+              },
+              onValueChanged: (value) {
+                setState(() {
+                  rating = value!;
+                });
+              },
+              thumbColor: theme.primary,
+              backgroundColor: theme.backgroundSecondary,
+              padding: EdgeInsets.all(Dimension.radius.four),
             ),
             BlocBuilder<FindBusinessesByCategoryBloc, FindBusinessesByCategoryState>(
               builder: (context, state) {
@@ -257,51 +321,55 @@ class _FilterBusinessesByCategoryWidgetState extends State<FilterBusinessesByCat
                       padding: const EdgeInsets.all(0),
                       physics: const NeverScrollableScrollPhysics(),
                       children: [
-                        const Divider(height: 42),
+                        const Divider(height: 24),
                         Text(
                           'Related',
                           style: TextStyles.subTitle(context: context, color: theme.textPrimary),
                         ),
                         const SizedBox(height: 8),
                         Container(
+                          constraints: BoxConstraints(
+                            minHeight: Dimension.size.vertical.min,
+                            maxHeight: Dimension.size.vertical.hundred,
+                          ),
                           decoration: BoxDecoration(
                             color: theme.backgroundSecondary,
                             borderRadius: BorderRadius.circular(16),
                             border: Border.all(color: theme.backgroundTertiary, width: .25),
                           ),
                           padding: const EdgeInsets.all(8),
-                          child: Wrap(
-                            spacing: 8,
-                            runSpacing: 6,
-                            alignment: WrapAlignment.start,
-                            crossAxisAlignment: WrapCrossAlignment.start,
-                            direction: Axis.horizontal,
-                            runAlignment: WrapAlignment.start,
-                            verticalDirection: VerticalDirection.down,
-                            children: state.related
-                                .map(
-                                  (category) => ActionChip(
-                                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                                    backgroundColor: theme.backgroundPrimary,
-                                    padding: const EdgeInsets.all(12),
-                                    side: BorderSide(color: theme.backgroundTertiary, width: 1),
-                                    visualDensity: const VisualDensity(horizontal: -4, vertical: -4),
-                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-                                    label: Text(
-                                      category.name.full,
-                                      style: TextStyles.body(context: context, color: theme.textPrimary).copyWith(
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                    ),
-                                    onPressed: () {
-                                      context.pop();
-                                      context.pushNamed(SubCategoryPage.name, pathParameters: {
-                                        'urlSlug': category.urlSlug,
-                                      });
-                                    },
+                          child: MasonryGridView.count(
+                            crossAxisCount: 3,
+                            mainAxisSpacing: Dimension.padding.horizontal.large,
+                            crossAxisSpacing: Dimension.padding.vertical.small,
+                            padding: EdgeInsets.zero,
+                            itemCount: state.related.length,
+                            scrollDirection: Axis.horizontal,
+                            clipBehavior: Clip.antiAlias,
+                            shrinkWrap: true,
+                            itemBuilder: (_, index) {
+                              final category = state.related[index];
+                              return ActionChip(
+                                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                backgroundColor: theme.backgroundPrimary,
+                                padding: const EdgeInsets.all(12),
+                                side: BorderSide(color: theme.backgroundTertiary, width: 1),
+                                visualDensity: const VisualDensity(horizontal: -4, vertical: -4),
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+                                label: Text(
+                                  category.name.full,
+                                  style: TextStyles.body(context: context, color: theme.textPrimary).copyWith(
+                                    fontWeight: FontWeight.w400,
                                   ),
-                                )
-                                .toList(),
+                                ),
+                                onPressed: () {
+                                  context.pop();
+                                  context.pushNamed(SubCategoryPage.name, pathParameters: {
+                                    'urlSlug': category.urlSlug,
+                                  });
+                                },
+                              );
+                            },
                           ),
                         ),
                       ],
@@ -323,7 +391,13 @@ class _FilterBusinessesByCategoryWidgetState extends State<FilterBusinessesByCat
                               district: district,
                               thana: thana,
                               subCategory: subCategory,
-                              ratings: ratings,
+                              ratings: rating == RatingRange.all
+                                  ? []
+                                  : rating == RatingRange.worst
+                                      ? [1, 2]
+                                      : rating == RatingRange.average
+                                          ? [3, 4]
+                                          : [5],
                               category: state.category.urlSlug,
                             ),
                           );
